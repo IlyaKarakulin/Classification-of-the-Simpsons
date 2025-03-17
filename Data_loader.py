d90570b52eff4ef850a92a7368f72c5c6ebd05b2
Ilya <karakulinilia@gmail.com>
2025-03-17 21:48:07 +0700

    write first version SimpsonDataset

--- Data_loader.py
+++ Data_loader.py
@@ -1,42 +1,65 @@
+import os
+
 import torch
 from torch.utils.data import Dataset
 from sklearn.preprocessing import LabelEncoder
 import pickle
 from PIL import Image
 from torchvision import transforms
+from pathlib import Path
 
 
-class DatasetLoader(Dataset):
-    def __init__(self, files):
+class SimpsonDataset(Dataset):
+    def __init__(self, path_to_files, mode='train'):
         super().__init__()
-        self.files = files
+
+        self.mode = mode
+        self.path_to_files = Path(path_to_files)
+        self.files = list(self.path_to_files.rglob('*.jpg'))
         self._len = len(self.files)
+
         self.label_encoder = LabelEncoder()
 
         self.labels = [path.parent.name for path in self.files]
         self.label_encoder.fit(self.labels)
 
-        with open('data/label_encoder.pkl', 'wb') as le_dump_file:
-            pickle.dump(self.label_encoder, le_dump_file)
-
-    def __getitem__(self, index):
-
-        x, size = self.load_sample(self.files[index])
-
-        transforms_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-
-
-
-    def __len__(self):
-        return self.len_
+        os.makedirs('./meta_data', exist_ok=True)
+        with open('./meta_data/label_encoder_conf.pkl', 'wb') as le_conf:
+            pickle.dump(self.label_encoder, le_conf)
 
     def load_sample(self, file):
         image = Image.open(file)
         image.load()
 
-        image = image.resize((self.rescale_size, self.rescale_size))
-
         return image, image.size
+    
+    def __getitem__(self, index):
+
+        x, _ = self.load_sample(self.files[index])
+
+        transforms_img = transforms.Compose([
+            transforms.Resize(256),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+        x = transforms_img(x)
+
+        if self.mode == 'train':
+            label_id = self.label_encoder.transform([self.labels[index]])
+            y = label_id.item()
+            return x, y
+        
+        elif self.mode == 'test':
+            return  x
+
+    def __len__(self):
+        return self._len
+    
+
+# path = "/home/ilya/.cache/kagglehub/datasets/alexattia/the-simpsons-characters-dataset/versions/4/simpsons_dataset/simpsons_dataset"
+# dataset = SimpsonDataset(path)
+
+# tmp, y = dataset.__getitem__(10)
+# print(tmp.shape)
+# print(tmp)
