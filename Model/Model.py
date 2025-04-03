066c1b161d949a6e684196e3bab2ac44eb156974
Ilia <karalulinilia@gmail.com>
2025-04-03 12:59:19 +0000

    new version model, -q MapP

--- Model/Model.py
+++ Model/Model.py
@@ -2,12 +2,9 @@ import os
 
 import torch.nn as nn
 import torch
-from torch.utils.data import random_split
 from torch.utils.data import DataLoader
-import torch.nn.functional as f
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-from torchsummary import summary
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle
 from tqdm import tqdm
@@ -70,7 +67,7 @@ class Model(nn.Module):
 
         self.conv7 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
-            nn.MaxPool2d(2),
+            # nn.MaxPool2d(2),
             nn.BatchNorm2d(256),
             nn.ReLU(),
         )
@@ -91,18 +88,18 @@ class Model(nn.Module):
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            # nn.AdaptiveAvgPool2d((1, 1))
+            nn.AdaptiveAvgPool2d((1, 1))
         )
 
         self.fc1 = nn.Sequential(
-            nn.Linear(5 * 5 * 256, 1024),
-            nn.BatchNorm1d(1024),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
             nn.Dropout(0.5),
             nn.ReLU()
         )
 
         self.fc2 = nn.Sequential(
-            nn.Linear(1024, n_classes),
+            nn.Linear(512, n_classes),
         )
 
 
@@ -119,7 +116,7 @@ class Model(nn.Module):
         x = self.conv10(x)
         # print(x.size())
 
-        x = x.view(x.size(0), 5 * 5 * 256)
+        x = x.view(x.size(0), 256)
 
         x = self.fc1(x)
         x = self.fc2(x)
