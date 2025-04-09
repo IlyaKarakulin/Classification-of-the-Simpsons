c77559f351fa3a776a862ff47d12bb2e5192fe25
Ilia <karalulinilia@gmail.com>
2025-04-09 13:37:55 +0000

    clear files

--- Model/Test.py
+++ Model/Test.py
@@ -1,15 +1,11 @@
 from Model import Classifier
 from Model import get_device
 
-path_to_train = "./dataset/train"
-path_to_val = "./dataset/val"
-path_to_test = "./dataset/test"
-
 device = get_device()
 model = Classifier(device)
 
-path_to_model = '/home/i.karakulin/Classification-of-the-Simpsons/meta_data/best.tar'
-path_to_test = '/home/i.karakulin/Classification-of-the-Simpsons/dataset/testset'
+path_to_model = './meta_data/best.tar'
+path_to_test = './dataset/testset'
 
 model.load_model(path_to_model)
 
---
+++ Model/Train.py
@@ -0,0 +1,21 @@
+from Model import Classifier
+from Model import get_device
+
+path_to_train = "./dataset/train"
+path_to_val = "./dataset/val"
+path_to_test = "./dataset/test"
+
+device = get_device()
+model = Classifier(device)
+
+# You can start learning from a pre-trained model
+# path_to_model = '/home/i.karakulin/Classification-of-the-Simpsons/meta_data/best.tar'
+# model.load_model(path_to_model)
+
+model.train(
+    path_to_train=path_to_train,
+    path_to_val=path_to_val,
+    num_epoch=60,
+    batch_size=128,
+    lr=0.003
+)
